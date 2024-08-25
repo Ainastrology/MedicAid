7200d3cb1180fe2c6c2ee50e9bbbde49058896be
Qurat-ul-ain <qurat1356@gmail.com>
2024-08-25 14:39:59 +0500

    Pharma2 Update for Map integration with its props

--- client/src/pages/Pharma2.js
+++ client/src/pages/Pharma2.js
@@ -8,7 +8,7 @@ function Pharma() {
       initialViewState={{
         longitude: -100,
         latitude: 40,
-        zoom: 3.5,
+        zoom: 12,
       }}
       style={{ width: 600, height: 400 }}
       mapStyle="mapbox://styles/mapbox/streets-v9"
